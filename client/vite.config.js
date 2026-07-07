66cfd46ca6ba67a99e700113b13efc13fc5e24b3
888sam-b <bsam53888@gmailcom>
2026-07-07 20:29:07 +0530

    bug-fix1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
+
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5000',
+          target: env.VITE_API_URL || 'https://nss-form.onrender.com',
           changeOrigin: true,
           secure: false
         }
